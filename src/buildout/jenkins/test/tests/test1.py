00749f5fe06cc73e4d7ac2789d5bb01920e63222
oxydedefer <sirtoli.dev@gmail.com>
2018-11-22 16:52:48 +0100

    Fix test

--- src/buildout/jenkins/test/tests/test1.py
+++ src/buildout/jenkins/test/tests/test1.py
@@ -4,7 +4,7 @@ import unittest
 class TestArithmetic(unittest.TestCase):
 
     def test_two_plus_two(self):
-        self.assertEqual(2 + 2, 5)
+        self.assertEqual(2 + 2, 4)
 
 
 def test_suite():
